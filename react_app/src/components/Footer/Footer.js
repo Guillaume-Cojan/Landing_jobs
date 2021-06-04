cb4c04b25ddf268e7c04f05fd90206b9eee4b69a
Anna Gosme <anna.gosme@gmail.com>
2021-06-04 12:18:26 +0200

    create footer folder footer layout and footer css, need to position with flex/grid

---
+++ react_app/src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import Onion from "../assets/Onion.svg";
+function Footer() {
+    return (
+    <div className="footer-wrapper">
+  <h3 className="footer-logo"><img className="footer-logo-img" src={Onion} alt="logo"></img>
+  &nbsp; LaslesVPN</h3>
+ <p><strong>LaslesVPN</strong> is a private virtual network that has unique features and has high security.</p>
+    <div className="big-dot"></div>
+    <div className="big-dot"></div>
+    <div className="big-dot"></div>
+<h4 className="earn-money">Earn Money</h4>
+<p>Affiliate</p>
+<p>Become a Partner</p>
+<h5>&copy; LaslesVPN</h5>
+    </div>
+    )
+}
+
+export default Footer;
