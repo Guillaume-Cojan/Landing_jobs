e75ce5d3892ee47e63c0a7bb11ea8180f5c158b3
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-06-08 17:58:11 +0300

    create initial state for benchmark component

--- react_app/src/components/Job/JobList.js
+++ react_app/src/components/Job/JobList.js
@@ -1,24 +1,28 @@
 import JobCard from "./JobCard";
-import "./JobList.css"
-
+import "./JobList.css";
 
 const JobList = () => {
-    
-    return(
-        <div className="job-list-container" >
-            <h1 className="job-list-title">
-                Jobs for UX Designer
-            </h1>
+    return (
+        <div className="job-list-container">
+            <h1 className="job-list-title">Jobs for UX Designer</h1>
             <div className="job-items">
                 <JobCard />
                 <JobCard />
                 <JobCard />
             </div>
             <p className="job-list-link">
-                View more jobs on <a href="https://landing.jobs/"> Landing.Jobs </a>
+                View more jobs on{" "}
+                <a
+                    href="https://landing.jobs/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {" "}
+                    Landing.Jobs{" "}
+                </a>
             </p>
         </div>
-    )
-}
+    );
+};
 
 export default JobList;
