1f04b499005767278beec21ede807d381b9fe117
Anca <anca96laura21@yahoo.com>
2021-06-09 10:54:02 +0300

    checkup

--- react_app/src/components/Begining/Intro.js
+++ react_app/src/components/Begining/Intro.js
@@ -1,23 +1,26 @@
-import Onion from "../assets/Onion.svg";
-import FeedbackSession1 from "..assets/Allura-FeedbackSession1.svg";
+import Onion from "./../assets/Onion.svg";
+import Allura from "./../assets/Allurra-FeedbackSession1.svg"
 import "./Intro.css";
+
+
 function Intro() {
     return (
-    <div className="rectangle-328">
-        <div className="top-logo">
-            <h3 className="logo"><img className="onion" src={Onion} alt="logo"></img> &nbsp; LaslesVPN</h3>
+        <div className="intro-container">
+            <div className="top-logo">
+                <h1 className="logo">&nbsp; LaslesVPN</h1>
+                <img className="onion" src={Onion} alt="logo"></img>
+            </div>
+            <div className="right-side">
+            <div className="content-intro">
+                <h1>Tech Professionals Salary Benchmarking</h1>
+                <p> Provide a network for all your needs with ease and fun using LaslesVPN discover interesting features from us. </p>
+            </div>
+            </div>
+            <div className="left-side">
+            <div className="intro-image"><img className="session" src={Allura} alt="session"></img>
+            </div>
+            </div>
         </div>
-        <div className="right-side">
-        <div className="content-intro">
-            <h1>Tech Professionals Salary Benchmarking</h1>
-            <p> Provide a network for all your needs with ease and fun using LaslesVPN discover interesting features from us. </p>
-        </div>
-        </div>
-        <div className="left-side">
-           <div className="intro-image"><img className="session" src={FeedbackSession1} alt="session"></img>
-           </div>
-        </div>
-    </div>
       )
   }
   
