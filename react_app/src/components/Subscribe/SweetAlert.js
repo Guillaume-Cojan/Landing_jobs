c75ec807c68ce26740803233bb579a779bb12ade
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-06-19 15:56:26 +0300

    connect subscribe component to db subscribers table

--- react_app/src/components/Subscribe/SweetAlert.js
+++ react_app/src/components/Subscribe/SweetAlert.js
@@ -6,6 +6,9 @@ export default class Sweetalertdemo extends Component {
     constructor() {
         super();
         this.HandleClick = this.HandleClick.bind(this);
+        /*this.state = {
+            postEmail: null,
+        };*/
     }
 
     HandleClickAutoclose() {
@@ -13,7 +16,7 @@ export default class Sweetalertdemo extends Component {
         Swal.fire({
             title: "Thank You!",
             html: "You were succesfully added to our mailing list.",
-            timer: 2400,
+            timer: 2500,
             timerProgressBar: true,
             onBeforeOpen: () => {
                 Swal.showLoading();
@@ -47,7 +50,25 @@ export default class Sweetalertdemo extends Component {
             showCancelButton: true,
             confirmButtonText: "Subscribe",
             confirmButtonColor: "#F53838",
-            showLoaderOnConfirm: true,
+            showLoaderOnConfirm: false,
+            preConfirm: (emailInput) => {
+                const requestOptions = {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        email: emailInput,
+                    }),
+                };
+                return fetch(
+                    `http://localhost:3000/subscribe`,
+                    requestOptions
+                ).catch((error) => {
+                    console.log(error);
+                    Swal.showValidationMessage(
+                        `Unable to subscribe now. Please try again later.`
+                    );
+                });
+            },
             allowOutsideClick: () => !Swal.isLoading(),
         }).then((result) => {
             if (result.isConfirmed) {
