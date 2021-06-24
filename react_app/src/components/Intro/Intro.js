5e14b7aa4435a28e2b130d2ed339ff9af62cd739
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-06-24 18:46:58 +0300

    rebuild the intro component css

--- react_app/src/components/Intro/Intro.js
+++ react_app/src/components/Intro/Intro.js
@@ -1,36 +1,32 @@
-import Onion from "./../../assets/Onion.svg";
-import Allura from "./../../assets/Allurra-FeedbackSession1.svg"
-import Ellipse from "./../../assets/Ellipse.svg"
+import LjLogo from "./../../assets/LJ_Logo.svg";
+import Allura from "./../../assets/Allurra-FeedbackSession1.svg";
+import Ellipse from "./../../assets/Ellipse.svg";
 import "./Intro.css";
-
 
 function Intro() {
     return (
         <div className="intro-container">
             <div className="top-logo">
-                <img className="onion" src={Onion} alt="logo"></img>
-                <h1 className="logo"> &nbsp; Landing</h1><h1  className="salary">SALARY</h1> 
+                <img className="lj-logo" src={LjLogo} alt="logo"></img>
             </div>
             <div className="intro-content">
-                <div className="left-side">
+                <div className="intro-text">
+                    <h3 className="intro-app">SALARY</h3>
                     <h2 className="intro-title">Tech Professionals</h2>
-                     <h2 className="intro-title">Salary Benchmarking</h2>
-                    <p className="intro-description"> Provide a network for all your needs with ease and fun using LaslesVPN discover interesting features from us. </p>
+                    <h2 className="intro-title">Salary Benchmarking</h2>
+                    <p className="intro-description">
+                        {" "}
+                        Provide a network for all your needs with ease and fun
+                        using LaslesVPN discover interesting features from us.{" "}
+                    </p>
                 </div>
-                <div className="right-side">
-                    <div className="intro-image">
-                        <img className="ellipse" src={Ellipse} alt="ellipse"/>
-                        <img className="session" src={Allura} alt="session">
-                        </img>
-
-                    </div>
+                <div className="intro-image">
+                    <img className="ellipse" src={Ellipse} alt="ellipse" />
+                    <img className="session" src={Allura} alt="session"></img>
                 </div>
             </div>
-
-
         </div>
-      )
-  }
-  
-  export default Intro;
+    );
+}
 
+export default Intro;
