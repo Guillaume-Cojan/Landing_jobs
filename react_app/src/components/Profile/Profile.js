73a7bdbe412809f31d36b8b29b2560304a84c7fb
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-06-25 12:18:23 +0300

    change style of profile cards

--- react_app/src/components/Profile/Profile.js
+++ react_app/src/components/Profile/Profile.js
@@ -1,38 +1,48 @@
 import "./Profile.css";
 
 const Profile = ({
-  title,
-  content,
-  image,
-  showLJ,
-  setShowLJ,
-  id,
-  setShowJobs,
+    title,
+    content,
+    image,
+    showLJ,
+    setShowLJ,
+    id,
+    setShowJobs,
 }) => {
-  const handleShowLJ = () => {
-    setShowLJ(true);
-    if (id === 1) {
-      setShowJobs(true);
-    } else {
-      setShowJobs(false);
-    }
-  };
+    const handleShowLJ = () => {
+        setShowLJ(true);
+        if (id === 1) {
+            setShowJobs(true);
+        } else {
+            setShowJobs(false);
+        }
+    };
 
-  return (
-    <div className="profile-card">
-      <div className="profile-text">
-        <h2 className="profile-title"> {title} </h2>
-        <p className="profile-content"> {content} </p>
-        <button className="bttn-get-started" onClick={() => handleShowLJ()}>
-          {" "}
-          Get Started{" "}
-        </button>
-      </div>
-      <div className="profile-right-side">
-        <img className="profile-img" src={image} alt="worker" />
-      </div>
-    </div>
-  );
+    return (
+        <div className="profile-card">
+            <div className="profile-text">
+                <h2
+                    className={id === 1 ? "profile-title-cie" : "profile-title"}
+                >
+                    {" "}
+                    {title}{" "}
+                </h2>
+                <p className="profile-content"> {content} </p>
+                <button
+                    className={
+                        id === 1 ? "bttn-get-started-cie" : "bttn-get-started"
+                    }
+                    onClick={() => handleShowLJ()}
+                >
+                    {" "}
+                    {id === 1 ? "I'm Companie" : "I'm Talent"}{" "}
+                </button>
+            </div>
+            <div className="profile-right-side">
+                <img className="profile-img" src={image} alt="worker" />
+            </div>
+        </div>
+    );
 };
 
 export default Profile;
