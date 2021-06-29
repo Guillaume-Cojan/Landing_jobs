0724964f3085513fa4be98ac2d48d32402d7fa17
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-06-29 15:08:00 +0300

    add all links to footer

--- react_app/src/components/Footer/Footer.js
+++ react_app/src/components/Footer/Footer.js
@@ -51,17 +51,89 @@ function Footer() {
             </div>
             <div className="footer-center">
                 <h4 className="footer-title">RESOURCES</h4>
-                <p>Reading Materials</p>
-                <p>Blog</p>
-                <p>Press Kit</p>
+                <p>
+                    <a
+                        href="https://landing.jobs/resources/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="footer-link"
+                    >
+                        Reading Materials
+                    </a>
+                </p>
+                <p>
+                    <a
+                        href="https://blog.landing.jobs/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="footer-link"
+                    >
+                        Blog
+                    </a>
+                </p>
+                <p>
+                    <a
+                        href="https://drive.google.com/drive/folders/1zhIwaq5nvGeFqo6ML80HC5ZK2StC5J8n"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="footer-link"
+                    >
+                        Press Kit
+                    </a>
+                </p>
             </div>
             <div className="footer-right">
                 <h4 className="footer-title">HELP</h4>
-                <p>About</p>
-                <p>Contacts</p>
-                <p>FAQ</p>
-                <p>Privacy</p>
-                <p>Terms</p>
+                <p>
+                    <a
+                        href="https://landing.jobs/faq"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="footer-link"
+                    >
+                        About
+                    </a>
+                </p>
+                <p>
+                    <a
+                        href="https://landing.jobs/contacts"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="footer-link"
+                    >
+                        Contacts
+                    </a>
+                </p>
+                <p>
+                    <a
+                        href="https://landing.jobs/faq"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="footer-link"
+                    >
+                        FAQ
+                    </a>
+                </p>
+                <p>
+                    <a
+                        href="https://landing.jobs/privacy"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="footer-link"
+                    >
+                        Privacy
+                    </a>
+                </p>
+                <p>
+                    <a
+                        href="https://landing.jobs/tos"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="footer-link"
+                    >
+                        Terms
+                    </a>
+                </p>
             </div>
             <div className="end-footer-mobile">
                 <div className="social-icons">
