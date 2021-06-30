e519a4bd47cb63a5f2babe6dac93a648a7b6ab99
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-06-30 20:58:56 +0300

    connect fetch API to job cards

--- react_app/src/components/LandingJobsContainer/LandingJobsList.js
+++ react_app/src/components/LandingJobsContainer/LandingJobsList.js
@@ -4,30 +4,98 @@ import "./LandingJobsList.css";
 import { useState, useEffect } from "react";
 
 const LandingJobsList = ({ showLJ, showJobs }) => {
-    const [job, setJob] = useState("");
-    const [searchResult, setSearchResult] = useState([]);
+    const [jobsList, setJobsList] = useState([]);
+    const [companiesList, setCompaniesList] = useState([]);
 
     const searchJob = () => {
         fetch("https://landing.jobs/api/v1/jobs")
             .then((res) => res.json())
-            .then((data) => setSearchResult(data));
+            .then((data) => setJobsList(data));
     };
-
     useEffect(searchJob, []);
 
-    console.log(searchResult);
+    const searchCompany = () => {
+        fetch("https://landing.jobs/api/v1/companies")
+            .then((res) => res.json())
+            .then((data) => setCompaniesList(data));
+    };
+    useEffect(searchCompany, []);
 
     return (
         <div className={showLJ ? "landingjobs-list-container " : "hidden"}>
             <h1 className="landingjobs-list-title">
-                {showJobs
-                    ? "Find the perfect candidates for UX Designer"
-                    : "Jobs for UX Designer"}
+                {showJobs ? "Find the perfect candidates" : "Latest Job Offers"}
             </h1>
             <div className="landingjobs-items">
-                {showJobs ? <TalentCard /> : <JobCard />}
-                {showJobs ? <TalentCard /> : <JobCard />}
-                {showJobs ? <TalentCard /> : <JobCard />}
+                {showJobs ? (
+                    <TalentCard />
+                ) : (
+                    <JobCard
+                        logo={
+                            companiesList.find(
+                                (x) =>
+                                    x.id ===
+                                    jobsList[jobsList.length - 1].company_id
+                            ).logo_url
+                        }
+                        title={jobsList[jobsList.length - 1].title}
+                        companyName={
+                            companiesList.find(
+                                (x) =>
+                                    x.id ===
+                                    jobsList[jobsList.length - 1].company_id
+                            ).name
+                        }
+                        city={jobsList[jobsList.length - 1].city}
+                        url={jobsList[jobsList.length - 1].url}
+                    />
+                )}
+                {showJobs ? (
+                    <TalentCard />
+                ) : (
+                    <JobCard
+                        logo={
+                            companiesList.find(
+                                (x) =>
+                                    x.id ===
+                                    jobsList[jobsList.length - 2].company_id
+                            ).logo_url
+                        }
+                        title={jobsList[jobsList.length - 2].title}
+                        companyName={
+                            companiesList.find(
+                                (x) =>
+                                    x.id ===
+                                    jobsList[jobsList.length - 2].company_id
+                            ).name
+                        }
+                        city={jobsList[jobsList.length - 2].city}
+                        url={jobsList[jobsList.length - 2].url}
+                    />
+                )}
+                {showJobs ? (
+                    <TalentCard />
+                ) : (
+                    <JobCard
+                        logo={
+                            companiesList.find(
+                                (x) =>
+                                    x.id ===
+                                    jobsList[jobsList.length - 3].company_id
+                            ).logo_url
+                        }
+                        title={jobsList[jobsList.length - 3].title}
+                        companyName={
+                            companiesList.find(
+                                (x) =>
+                                    x.id ===
+                                    jobsList[jobsList.length - 3].company_id
+                            ).name
+                        }
+                        city={jobsList[jobsList.length - 3].city}
+                        url={jobsList[jobsList.length - 3].url}
+                    />
+                )}
             </div>
             <p className="job-list-link">
                 View more on{" "}
