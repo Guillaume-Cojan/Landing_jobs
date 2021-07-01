573aa5fb7cb40a117b500a82d1e152d2fa5f58c7
Anca <anca96laura21@yahoo.com>
2021-07-01 22:56:08 +0300

    receiving formInput on backend

--- model/salarybenchmark.js
+++ model/salarybenchmark.js
@@ -3,9 +3,25 @@ const connection = require("../config");
 const Salary = {};
 
 let jobRole = "Back-End Developer";
-let experience = "0 - 3 years";
+let experience = "0 Years";
 let location = "Porto";
 
+Salary.addDataSalary = (userInput, callback) => {
+    console.log(userInput.formSelection);
+    jobRole = userInput.formSelection.Category;
+    experience = userInput.formSelection.Experience;
+    location = userInput.formSelection.Location;
+
+    let sqlQuery = "SELECT Job_Role, Working_Experience_Aggregated, Work_Company_PT_District FROM salarybenchmark WHERE job_role = ? AND Working_Experience_Aggregated = ? AND Work_Company_PT_District = ? "
+    let sqlValues = []
+    sqlValues.push(userInput.Job_Role, userInput.Working_Experience_Aggregated, userInput.Work_Company_PT_District)
+    connection.query(sqlQuery, sqlValues, (err, result) => {
+        callback(err, result)
+        });
+        };
+
+      
+     
 Salary.getDataSalary = (callback) => {
     connection.query('SELECT AVG(Avg_Salary) AS average_salary FROM salarybenchmark WHERE job_role = ? AND Working_Experience_Aggregated = ? AND Work_Company_PT_District = ? ', [ jobRole, experience, location ], 
     (err, result) => {
@@ -28,16 +44,5 @@ Salary.getDataMaximumSalary = (callback) => {
     }
     )};
 
-
-Salary.addDataSalary = (userInput, callback) => {
-    console.log(userInput);
-    // let sql = "INSERT INTO salarybenchmark (`Work_Company_PT_District`, `Job_Role`, `Working_Experience_Aggregated`) VALUES (?)";
-    let sqlQuery = "SELECT ID, Job_Role, Working_Experience_Aggregated, Work_Company_PT_District FROM salarybenchmark WHERE job_role = ? AND Working_Experience_Aggregated = ? AND Work_Company_PT_District = ? "
-    let sqlValues = []
-    sqlValues.push(userInput.Job_Role, userInput.Working_Experience_Aggregated, userInput.Work_Company_PT_District)
-    connection.query(sqlQuery, sqlValues, (err, result) => {
-        callback(err, result);
-        });
-        };
 
 module.exports = Salary;
