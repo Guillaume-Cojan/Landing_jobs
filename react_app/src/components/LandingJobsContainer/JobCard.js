2fd6947b2e3e16b6ade29cd982f3b0479ec6518b
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-07-01 12:20:30 +0300

    restyle jobs and talents cards after API integration

--- react_app/src/components/LandingJobsContainer/JobCard.js
+++ react_app/src/components/LandingJobsContainer/JobCard.js
@@ -1,13 +1,14 @@
 function JobCard({ logo, title, companyName, city, url }) {
     return (
         <div className="job-card">
-            <img className="job-logo" src={logo} alt="company-logo" />
             <h2 className="job-title">{title}</h2>
-            <div className="company-location">
-                <h3 className="company-name">{companyName}</h3>
-                <h3 className="location">{city}</h3>
+            <div className="job-description">
+                <img className="job-logo" src={logo} alt="company-logo" />
+                <div className="company-location">
+                    <h3 className="company-name">{companyName}</h3>
+                    <h3 className="location">{city}</h3>
+                </div>
             </div>
-
             <form
                 className="bttn-job-details"
                 action={url}
