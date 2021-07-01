d5c9ed0de556b53fc136f3a5e3be2e0b90cbbd45
Anca <anca96laura21@yahoo.com>
2021-07-01 22:55:33 +0300

    frontend and backend connection

--- react_app/src/components/Benchmark/Survey.js
+++ react_app/src/components/Benchmark/Survey.js
@@ -1,4 +1,6 @@
+import React, { useState, useEffect } from "react";
 import SurveyList from "./SurveyList";
+
 
 const category = [
     "Back-End Developer",
@@ -48,11 +50,32 @@ const location = [
 ];
 
 function Survey({ showLJ, showJobs, setShowResult }) {
+    
+    const [ userSelection, setUserSelection] = useState({})
 
-    const handleCalculateClick = ()  => {
-        setShowResult(true)
-     } 
+    console.log("Right now selection is : ", userSelection)
 
+    const handleCalculateClick = () => {
+                setShowResult(true)
+             }
+
+    const sendSelectionToBE = () => {
+        if(userSelection.Category && userSelection.Experience && userSelection.Location){ fetch("http://localhost:5000/salarybenchmark/", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({formSelection: userSelection}),
+        })}
+        // else {
+        //     console.log("not yet")
+        // }
+    }
+   
+
+    useEffect( sendSelectionToBE, [userSelection])
+ 
+   
     return (
         <div className="survey-container">
             <h2
@@ -70,9 +93,9 @@ function Survey({ showLJ, showJobs, setShowResult }) {
                 You can explore the features that we provide with fun and have
                 their own functions each feature.
             </p>
-            <SurveyList title={"Category"} list={category} />
-            <SurveyList title={"Experience"} list={experience} />
-            <SurveyList title={"Location"} list={location} />
+            <SurveyList title={"Category"} list={category} setUserSelection={setUserSelection} userSelection={userSelection}/>
+            <SurveyList title={"Experience"} list={experience} setUserSelection={setUserSelection} userSelection={userSelection}/>
+            <SurveyList title={"Location"} list={location} setUserSelection={setUserSelection} userSelection={userSelection}/>
             <button
                 className={
                     showLJ
@@ -81,7 +104,7 @@ function Survey({ showLJ, showJobs, setShowResult }) {
                             : "calculate-btn-talent"
                         : "calculate-btn"
                 }
-                onClick={() => handleCalculateClick()}
+                onClick={handleCalculateClick}
             >
                 Calculate
             </button>
@@ -90,3 +113,72 @@ function Survey({ showLJ, showJobs, setShowResult }) {
 }
 
 export default Survey;
+
+
+
+
+
+
+
+
+
+
+//{ showLJ, showJobs, setShowResult }
+
+// class Survey extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             Category: "empty category",
+//             Experience: "empty experience",
+//             Location: "empty location"
+//         }
+
+//      } 
+     
+//      handleCalculateClick() {
+//         this.props.setShowResult(true)
+//      }
+
+
+
+//     render() {    
+//         const { showLJ, showJobs} = this.props;
+//     return (
+//         <div className="survey-container">
+//             <h2
+//                 className={
+//                     showLJ
+//                         ? showJobs
+//                             ? "survey-title-cie"
+//                             : "survey-title-talent"
+//                         : "survey-title"
+//                 }
+//             >
+//                 Salary Benchmarking
+//             </h2>
+//             <p className="survey-p">
+//                 You can explore the features that we provide with fun and have
+//                 their own functions each feature.
+//             </p>
+//             <SurveyList title={"Category"} list={category} state={this.state.C} setState={this.setState}/>
+//             <SurveyList title={"Experience"} list={experience} setState={this.setState}/>
+//             <SurveyList title={"Location"} list={location} setState={this.setState}/>
+//             <button
+//                 className={
+//                     showLJ
+//                         ? showJobs
+//                             ? "calculate-btn-cie"
+//                             : "calculate-btn-talent"
+//                         : "calculate-btn"
+//                 }
+//                 onClick={() => this.handleCalculateClick()}
+//             >
+//                 Calculate
+//             </button>
+//         </div>
+//     );}
+// }
+
+
+// export default Survey;
--- react_app/src/components/Benchmark/SurveyList.js
+++ react_app/src/components/Benchmark/SurveyList.js
@@ -1,17 +1,94 @@
+import React from "react";
 import Select from "react-select";
 
-function SurveyList({ title, list }) {
+
+
+function SurveyList({ title, list, setUserSelection, userSelection }) {
+    
     const options = [];
 
     list.forEach(function (element) {
         options.push({ label: element, value: element });
     });
+
+    const handleSelectChanges = (e, name) => {
+        setUserSelection({...userSelection, [name.name] : e.value})
+    }
+
     return (
         <div className="survey-list-container">
             <h3>{title}</h3>
-            <Select options={options} className="select" />
+            <Select name={title} options={options} className="select" onChange={handleSelectChanges}/>
         </div>
     );
 }
+
+
+
+// class SurveyList extends React.Component {
+//     constructor(props){ 
+//         super(props)
+//         this.state = {
+//             userSelection: "hellor"
+ 
+//         }
+//     }
+
+
+// //    handleSelectChanges = ( e, name) => {
+    
+// //     const handleCategoryChange = (e) => {
+// //         this.setState({
+// //             userSelection: { ...this.state.userSelection, [name.name]: e.value},
+// //           });
+// //     }
+    
+
+//     // }   
+//     // { [name.name] : e.value}
+//     // console.log("this is e " , e )
+//     // console.log("this is name " , name)
+
+
+//     // handleSelectChanges = ( e, name) => {
+//     //     if(name.name === "Category") {
+//     //         this.setState(
+//     //     { Category : e.value, Experience: this.state.userSelection.Experience})
+//     //     }
+//     //     if(name.name === "Experience"){
+//     //         this.setState(
+//     //             {Category : this.state.userSelection.Category, Experience : e.value})
+//     //     }
+//     // }   
+
+//     handleSelectChanges = (e, name, state, setState) => {
+//         console.log("hello from", name.name)
+//     }
+
+//     componentDidUpdate(){
+//         console.log(this.props.state)
+//     }
+
+//   render () {  
+//     const {title, list, state, setState } = this.props;
+
+//     const options = [];
+
+//     list.forEach(function (element) {
+//         options.push({ label: element, value: element });
+//     });
+
+//      console.log( setState)
+//     return (
+//         <div className="survey-list-container">
+//             <h3>{title}</h3>
+//             <Select name={title} options={options} className="select" state={state} setState={setState} onChange={this.handleSelectChanges}/>
+//         </div>
+//     );
+// }
+
+
+// }
+
 
 export default SurveyList;
