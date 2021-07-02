1d04c42d591936ed2e932a4c3906e6743f1a56c4
Anca <anca96laura21@yahoo.com>
2021-07-02 23:55:17 +0300

    fix routes for salarybenchmark

--- backend/model/salarybenchmark.js
+++ backend/model/salarybenchmark.js
@@ -2,19 +2,16 @@ const connection = require("../config");
 
 const Salary = {};
 
-let jobRole = "Back-End Developer";
-let experience = "0 Years";
-let location = "Porto";
 
 Salary.addDataSalary = (userInput, callback) => {
-    console.log(userInput.formSelection);
-    jobRole = userInput.formSelection.Category;
-    experience = userInput.formSelection.Experience;
-    location = userInput.formSelection.Location;
+
+    jobRole = userInput.formSelection.category;
+    experience = userInput.formSelection.experience;
+    location = userInput.formSelection.location;
 
     let sqlQuery = "SELECT Job_Role, Working_Experience_Aggregated, Work_Company_PT_District FROM salarybenchmark WHERE job_role = ? AND Working_Experience_Aggregated = ? AND Work_Company_PT_District = ? "
     let sqlValues = []
-    sqlValues.push(userInput.Job_Role, userInput.Working_Experience_Aggregated, userInput.Work_Company_PT_District)
+    sqlValues.push(jobRole, experience, location)
     connection.query(sqlQuery, sqlValues, (err, result) => {
         callback(err, result)
         });
@@ -22,23 +19,23 @@ Salary.addDataSalary = (userInput, callback) => {
 
       
      
-Salary.getDataSalary = (callback) => {
-    connection.query('SELECT AVG(Avg_Salary) AS average_salary FROM salarybenchmark WHERE job_role = ? AND Working_Experience_Aggregated = ? AND Work_Company_PT_District = ? ', [ jobRole, experience, location ], 
+Salary.getDataSalary = (formInput, callback) => {
+    connection.query('SELECT AVG(Avg_Salary) AS average_salary FROM salarybenchmark WHERE job_role = ? AND Working_Experience_Aggregated = ? AND Work_Company_PT_District = ? ', [ formInput.category, formInput.experience, formInput.location ], 
     (err, result) => {
        callback(err, result);
     }
   );
 };
 
-Salary.getDataMinimumSalary = (callback) => {
-    connection.query('SELECT Avg_Salary AS minimum_salary FROM salarybenchmark WHERE job_role = ? AND Working_Experience_Aggregated = ? AND Work_Company_PT_District = ? ORDER BY Avg_Salary ASC LIMIT 1', [ jobRole, experience, location ], 
+Salary.getDataMinimumSalary = (formInput, callback) => {
+    connection.query('SELECT Avg_Salary AS minimum_salary FROM salarybenchmark WHERE job_role = ? AND Working_Experience_Aggregated = ? AND Work_Company_PT_District = ? ORDER BY Avg_Salary ASC LIMIT 1', [ formInput.category, formInput.experience, formInput.location ], 
     (err, result) => {
         callback(err, result);
     }
     )};
 
-Salary.getDataMaximumSalary = (callback) => {
-    connection.query('SELECT Avg_Salary AS maximum_salary FROM salarybenchmark WHERE job_role = ? AND Working_Experience_Aggregated = ? AND Work_Company_PT_District = ? ORDER BY Avg_Salary DESC LIMIT 1', [ jobRole, experience, location ], 
+Salary.getDataMaximumSalary = (formInput, callback) => {
+    connection.query('SELECT Avg_Salary AS maximum_salary FROM salarybenchmark WHERE job_role = ? AND Working_Experience_Aggregated = ? AND Work_Company_PT_District = ? ORDER BY Avg_Salary DESC LIMIT 1', [ formInput.category, formInput.experience, formInput.location ], 
     (err, result) => {
          callback(err, result);
     }
--- backend/routes/route-salarybenchmark.js
+++ backend/routes/route-salarybenchmark.js
@@ -3,9 +3,9 @@ const connection = require("../config");
 const router = express.Router();
 const { getMinimumSalary, getMaximumSalary, getAverageSalary, receiveSalaryInput } = require("../controller/salarybenchmark-controller");
 
-router.post("/", receiveSalaryInput);
+router.post("/", receiveSalaryInput, getAverageSalary, getMinimumSalary, getMaximumSalary);
 
-router.get("/", getAverageSalary, getMinimumSalary, getMaximumSalary)
+// router.get("/", getAverageSalary, getMinimumSalary, getMaximumSalary)
 
 
 
