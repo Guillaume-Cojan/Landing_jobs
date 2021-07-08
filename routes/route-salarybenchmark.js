94dd7f1fbc28044af72269666c62a2cf3fb76bcc
Anna Gosme <anna.gosme@gmail.com>
2021-07-08 10:08:27 +0200

    add new fields Employer Industry and Employer Org Type

--- routes/route-salarybenchmark.js
+++ routes/route-salarybenchmark.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const connection = require("../config");
 const router = express.Router();
-const { getMinimumSalary, getMaximumSalary, getAverageSalary, receiveSalaryInput } = require("../controller/salarybenchmark-controller");
+const {
+  getMinimumSalary,
+  getMaximumSalary,
+  getAverageSalary,
+  receiveSalaryInput,
+} = require("../controller/salarybenchmark-controller");
 
-router.post("/", receiveSalaryInput, getAverageSalary, getMinimumSalary, getMaximumSalary);
+router.post(
+  "/",
+  receiveSalaryInput,
+  getAverageSalary,
+  getMinimumSalary,
+  getMaximumSalary
+);
 
 // router.get("/", getAverageSalary, getMinimumSalary, getMaximumSalary)
-
-
-
 
 module.exports = router;
