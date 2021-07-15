c55391b7790694db97fc046e1cbcb058a83da0a4
Anna Gosme <anna.gosme@gmail.com>
2021-07-15 13:02:06 +0200

    fix position of language select

--- react_app/src/components/Intro/Intro.js
+++ react_app/src/components/Intro/Intro.js
@@ -6,34 +6,37 @@ import { useTranslation } from "react-i18next";
 import cookies from "js-cookie";
 import LanguageSwitcher from "../LanguageSwitcher";
 
-function Intro({ }) {
+function Intro({}) {
   const currentLanguageCode = cookies.get("i18next") || "en";
-  const { t, i18n } = useTranslation(); 
+  const { t, i18n } = useTranslation();
   // i18next.options.supportedLngs
 
   return (
     <div className="intro-container">
+      <div className="top-grid">
+      <div className="top-logo">
+        <img className="lj-logo" src={LjLogo} alt="Landing.Jobs-logo"></img>
+        </div>
+        <div className="language-select">
       <LanguageSwitcher />
-        <div className="intro-content">
-          <div className="intro-text">
-            <h3 className="intro_app">{t("intro_app")}</h3>
-            <h2 className="intro_title">{t("intro_title")}</h2>
-            <h2 className="intro_title_2">{t("intro_title_2")}</h2>
-            <p className="intro_description">
-              {t(
-                "intro_description"
-              )}
-            </p>
-          </div>
-          <div className="intro-image">
-            <img
-              className="ellipse"
-              src={IntroImg}
-              alt="intro-talents-companies"
-            />
-          </div>
+      </div>
+      </div>
+      <div className="intro-content">
+        <div className="intro-text">
+          <h3 className="intro_app">{t("intro_app")}</h3>
+          <h2 className="intro_title">{t("intro_title")}</h2>
+          <h2 className="intro_title_2">{t("intro_title_2")}</h2>
+          <p className="intro_description">{t("intro_description")}</p>
+        </div>
+        <div className="intro-image">
+          <img
+            className="ellipse"
+            src={IntroImg}
+            alt="intro-talents-companies"
+          />
         </div>
       </div>
+    </div>
   );
 }
 
