7ddb6d062df07e1f5cc72818f021e0ee9450de90
Anca <anca96laura21@yahoo.com>
2021-07-16 10:53:23 +0300

    subscriber container fixed when landing jobs container present

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -17,9 +17,9 @@ function App() {
 
             <Intro />
             <ProfileSection setShowLJ={setShowLJ} setShowJobs={setShowJobs} />
-            {/* <Benchmark showLJ={showLJ} showJobs={showJobs} /> */}
+            <Benchmark showLJ={showLJ} showJobs={showJobs} />
             <LandingJobsList showLJ={showLJ} showJobs={showJobs} />
-            <Subscribe />
+            <Subscribe showLJ={showLJ}/>
             <Footer showLJ={showLJ} /> 
         </div>
     );
