b441bde9eb9363553b3e7a0bfa2079d02fa0fe9d
Anna Gosme <anna.gosme@gmail.com>
2021-07-18 00:04:51 +0200

    finalise reset button style

--- react_app/src/components/Benchmark/Survey.js
+++ react_app/src/components/Benchmark/Survey.js
@@ -3,103 +3,110 @@ import SurveyList from "./SurveyList";
 import surveyFields from "./SurveyFields";
 
 function Survey({ showLJ, showJobs, setShowResult, setGraphData }) {
-    const [userSelection, setUserSelection] = useState({
-        category: "notSelected",
-        experience: "notSelected",
-        location: "notSelected",
-        industry: "notSelected",
-        organisation: "notSelected",
-    });
+  const [userSelection, setUserSelection] = useState({
+    category: "notSelected",
+    experience: "notSelected",
+    location: "notSelected",
+    industry: "notSelected",
+    organisation: "notSelected",
+  });
 
-    const handleCalculateClick = () => {
-        const Swal = require("sweetalert2");
-        if (
-            (userSelection.category === "notSelected" &&
-                userSelection.experience === "notSelected") ||
-            userSelection.category === "notSelected" ||
-            userSelection.experience === "notSelected"
-        ) {
-            setShowResult(false);
-            Swal.fire({
-                icon: "info",
-                html: "Please select the <b>Category</b> and <b>Experience</b> fields!",
-                confirmButtonColor: "#3bbcb0",
-            });
-        } else {
-            setShowResult(true);
-            sendSelectionToBE();
+  const handleCalculateClick = () => {
+    const Swal = require("sweetalert2");
+    if (
+      (userSelection.category === "notSelected" &&
+        userSelection.experience === "notSelected") ||
+      userSelection.category === "notSelected" ||
+      userSelection.experience === "notSelected"
+    ) {
+      setShowResult(false);
+      Swal.fire({
+        icon: "info",
+        html: "Please select the <b>Category</b> and <b>Experience</b> fields!",
+        confirmButtonColor: "#3bbcb0",
+      });
+    } else {
+      setShowResult(true);
+      sendSelectionToBE();
+    }
+  };
+
+  const handleResetClick = () => {
+      console.log("clear button")
+    window.location.reload(false);
+  };
+
+  const sendSelectionToBE = () => {
+    if (
+      userSelection.category &&
+      userSelection.experience &&
+      userSelection.location &&
+      userSelection.industry &&
+      userSelection.organisation
+    ) {
+      fetch("https://landing-pay-server.herokuapp.com/salarybenchmark/", {
+        // mode: 'no-cors',
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ formSelection: userSelection }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setGraphData(data);
+        });
+    }
+  };
+
+  return (
+    <div className="survey-container">
+      <h2
+        className={
+          showLJ
+            ? showJobs
+              ? "survey-title-company"
+              : "survey-title-talent"
+            : "survey-title"
         }
-    };
-
-    const handleResetClick = () => {
-        window.location.reload(false);
-      };
-
-    const sendSelectionToBE = () => {
-        if (
-            userSelection.category &&
-            userSelection.experience &&
-            userSelection.location &&
-            userSelection.industry &&
-            userSelection.organisation
-        ) {
-            fetch("https://landing-pay-server.herokuapp.com/salarybenchmark/", {
-                // mode: 'no-cors',
-                method: "POST",
-                headers: {
-                    "Content-type": "application/json",
-                },
-                body: JSON.stringify({ formSelection: userSelection }),
-            })
-                .then((res) => res.json())
-                .then((data) => {
-                    setGraphData(data);
-                });
-        }
-    };
-
-    return (
-        <div className="survey-container">
-            <h2
-                className={
-                    showLJ
-                        ? showJobs
-                            ? "survey-title-company"
-                            : "survey-title-talent"
-                        : "survey-title"
-                }
-            >
-                Salary Benchmarking
-            </h2>
-            <p className="survey-p">Put the power of "pay" into your hands.</p>
-            {surveyFields.map((field, index) => (
-                <SurveyList
-                    key={index}
-                    title={field.title}
-                    list={field.list}
-                    setUserSelection={setUserSelection}
-                    userSelection={userSelection}
-                />
-            ))}
-            <div className="btn-container">
-            <button
-                className={
-                    showLJ
-                        ? showJobs
-                            ? "calculate-btn-company"
-                            : "calculate-btn-talent"
-                        : "calculate-btn"
-                }
-                onClick={handleCalculateClick}
-            >
-                Calculate
-            </button>
-            <button className="btn-reset" onClick={handleResetClick}>
+      >
+        Salary Benchmarking
+      </h2>
+      <p className="survey-p">Put the power of "pay" into your hands.</p>
+      {surveyFields.map((field, index) => (
+        <SurveyList
+          key={index}
+          title={field.title}
+          list={field.list}
+          setUserSelection={setUserSelection}
+          userSelection={userSelection}
+        />
+      ))}
+      <div className="btn-container">
+        <button
+          className={
+            showLJ
+              ? showJobs
+                ? "calculate-btn-company"
+                : "calculate-btn-talent"
+              : "calculate-btn"
+          }
+          onClick={handleCalculateClick}
+        >
+          Calculate
+        </button>
+        <button className={  showLJ
+              ? showJobs
+                ? "cancel-btn-company"
+                : "cancel-btn-talent"
+              : "cancel-btn"
+          }
+           onClick={handleResetClick}>
           Clear fields
         </button>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Survey;
