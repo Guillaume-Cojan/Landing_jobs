5adc49ef81d2f0c43afbf3ddb52c02ea666f8abc
Anca <anca96laura21@yahoo.com>
2021-07-18 23:40:57 +0300

    language select css

--- react_app/src/components/Intro/Intro.js
+++ react_app/src/components/Intro/Intro.js
@@ -7,32 +7,28 @@ import LanguageSwitcher from "../LanguageSwitcher";
 function Intro() {
 const { t } = useTranslation();
     return (
-      <div className="intro-container">
-      <div className="top-grid">
-      <div className="top-logo">
-        <img className="lj-logo" src={LjLogo} alt="Landing.Jobs-logo"></img>
-        </div>
-        <div className="language-select">
-      <LanguageSwitcher />
-      </div>
-      </div>
+        <div className="intro-container">
+            <div className="top-logo">
+                <img className="lj-logo" src={LjLogo} alt="Landing.Jobs-logo"></img>
+                <LanguageSwitcher />
+            </div>
             <div className="intro-content">
-                <div className="intro-text">
- 
-                    <h3 className="intro-app">{t("intro_app")}</h3>
-                    <h2 className="intro-title">{t("intro_title")}</h2>
-                    <h2 className="intro-title">{t("intro_title_2")}</h2>
-                    <p className="intro-description">
-                    {t("intro_description")}
-                    </p>
-                </div>
-                <div className="intro-image">
-                    <img
-                        className="ellipse"
-                        src={IntroImg}
-                        alt="intro-talents-companies"
-                    />
-                </div>
+                    <div className="intro-text">
+    
+                        <h3 className="intro-app">{t("intro_app")}</h3>
+                        <h2 className="intro-title">{t("intro_title")}</h2>
+                        <h2 className="intro-title">{t("intro_title_2")}</h2>
+                        <p className="intro-description">
+                        {t("intro_description")}
+                        </p>
+                    </div>
+                    <div className="intro-image">
+                        <img
+                            className="ellipse"
+                            src={IntroImg}
+                            alt="intro-talents-companies"
+                        />
+                    </div>
             </div>
             <div className="intro-bottom-section"> </div>
         </div>
