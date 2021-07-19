f69c4d1a3b66cb3f40be90974feac59bcadc911b
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-07-19 15:57:38 +0300

    add pool method to mysql connections

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 //We use the mysql login details that are stored in .env here:
-const connection = mysql.createConnection({
+/*const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
@@ -13,6 +13,16 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME,
 });
 
-module.exports = connection;
+module.exports = connection;*/
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+module.exports = pool;
 
 //mysql://bda4e96307de82:f0ef2cb9@us-cdbr-east-04.cleardb.com/heroku_cb95d913c021053?reconnect=true
