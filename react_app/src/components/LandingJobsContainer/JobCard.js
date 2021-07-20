bc50300285260925b3f7b46a0a803b21da6e97b3
Anca <anca96laura21@yahoo.com>
2021-07-20 13:22:57 +0300

    restructure job cards

--- react_app/src/components/LandingJobsContainer/JobCard.js
+++ react_app/src/components/LandingJobsContainer/JobCard.js
@@ -5,10 +5,13 @@ function JobCard({ logo, title, companyName, city, url }) {
 
     return (
         <div className="job-card">
-            <h2 className="job-title">{title}</h2>
+           
             <div className="job-description">
-                <img className="job-logo" src={logo} alt="company logo" />
-                <div className="company-location">
+            <img className="job-logo" src={logo} alt="company logo" />
+              
+                
+                <div className="job-details">
+                    <h2 className="job-title">{title}</h2>
                     <h3 className="company-name">{companyName}</h3>
                     <h3 className="location">{city}</h3>
                 </div>
