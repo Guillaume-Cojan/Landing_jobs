fcfe7043ea4a94586081dcf50ec37532051e5201
Anca <anca96laura21@yahoo.com>
2021-07-26 22:32:51 +0300

    read me update

--- react_app/src/components/Subscribe/Subscribe.js
+++ react_app/src/components/Subscribe/Subscribe.js
@@ -13,7 +13,7 @@ function Subscribe({showLJ}) {
                 <p className="subscribe-para">{t("subscribe_para")}</p>
             </div>
             <div className="subscribe-btn-container">
-                <SweetAlert />
+                <SweetAlert showLJ={showLJ}/>
             </div>
         </div>
     );
