f3311d118c0da25f8555949b1a3c776b5555f79d
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-07-26 11:36:50 +0300

    make average salary rounded, clean backend code

--- backend/routes/route-salarybenchmark.js
+++ backend/routes/route-salarybenchmark.js
@@ -1,22 +1,18 @@
 const express = require("express");
-//const connection = require("../config");
-const pool = require("../config");
 const router = express.Router();
 const {
-  getMinimumSalary,
-  getMaximumSalary,
-  getAverageSalary,
-  receiveSalaryInput,
+    getMinimumSalary,
+    getMaximumSalary,
+    getAverageSalary,
+    receiveSalaryInput,
 } = require("../controller/salarybenchmark-controller");
 
 router.post(
-  "/",
-  receiveSalaryInput,
-  getAverageSalary,
-  getMinimumSalary,
-  getMaximumSalary
+    "/",
+    receiveSalaryInput,
+    getAverageSalary,
+    getMinimumSalary,
+    getMaximumSalary
 );
-
-// router.get("/", getAverageSalary, getMinimumSalary, getMaximumSalary)
 
 module.exports = router;
