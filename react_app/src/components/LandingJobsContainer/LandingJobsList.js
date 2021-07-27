b4ce19209d2586f7ea9a47625384a588ea66a2e8
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-07-27 16:34:29 +0200

    make text on talent cards buttons change according to device size

--- react_app/src/components/LandingJobsContainer/LandingJobsList.js
+++ react_app/src/components/LandingJobsContainer/LandingJobsList.js
@@ -5,7 +5,6 @@ import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { talentList } from "./TalentLists";
 
-
 const LandingJobsList = ({ showLJ, showJobs }) => {
     const [jobsList, setJobsList] = useState([]);
     const [companiesList, setCompaniesList] = useState([]);
@@ -18,50 +17,69 @@ const LandingJobsList = ({ showLJ, showJobs }) => {
     };
     useEffect(searchJob, []);
 
-   
-
     const searchCompany = () => {
-        let companyIds = jobsList.map(job => job.company_id)
-        companyIds.map(id => {
+        let companyIds = jobsList.map((job) => job.company_id);
+        companyIds.map((id) => {
             fetch("https://landing.jobs/api/v1/companies/" + id)
-            .then((res) => res.json())
-            .then((data) =>  setCompaniesList(prevState => [...prevState, data]))
-            return 1})
+                .then((res) => res.json())
+                .then((data) =>
+                    setCompaniesList((prevState) => [...prevState, data])
+                );
+            return 1;
+        });
     };
     useEffect(searchCompany, [jobsList]);
-
-    
 
     return (
         <div className={showLJ ? "landingjobs-list-container " : "hidden"}>
             <h1 className="landingjobs-list-title">
-                {showJobs ? t("find_the_right_candidates") : t("latest_job_offers")}
+                {showJobs
+                    ? t("find_the_right_candidates")
+                    : t("latest_job_offers")}
             </h1>
             <div className="landingjobs-items">
                 {showJobs
-                    ? talentList.map((talent, index) => (  <TalentCard 
-                        key={index}
-                        avatar={talent.avatar}
-                        name={talent.name}
-                        title={talent.title}
-                        experience={talent.experience} />)) 
-                    : 
-                    jobsList.map((job, index) => ( <JobCard
-                        key={index}
-                        title={job.title}
-                        logo={ companiesList.length > 1 ? companiesList.find((x) => x.id === job.company_id).logo_url : "logo" }
-                        companyName={ companiesList.length > 1 ?companiesList.find((x) => x.id === job.company_id).name : "Loading..."}
-                        city={job.city}
-                        url={job.url}/> 
-                    ))
-                }
+                    ? talentList.map((talent, index) => (
+                          <TalentCard
+                              key={index}
+                              avatar={talent.avatar}
+                              name={talent.name}
+                              title={talent.title}
+                              experience={talent.experience}
+                          />
+                      ))
+                    : jobsList.map((job, index) => (
+                          <JobCard
+                              key={index}
+                              title={job.title}
+                              logo={
+                                  companiesList.length > 1
+                                      ? companiesList.find(
+                                            (x) => x.id === job.company_id
+                                        ).logo_url
+                                      : "logo"
+                              }
+                              companyName={
+                                  companiesList.length > 1
+                                      ? companiesList.find(
+                                            (x) => x.id === job.company_id
+                                        ).name
+                                      : "Loading..."
+                              }
+                              city={job.city}
+                              url={job.url}
+                          />
+                      ))}
             </div>
-            <p className="job-list-link"> {t("view_more_on")}{" "}
+            <p className="job-list-link">
+                {" "}
+                {t("view_more_on")}{" "}
                 <a
                     className={showJobs ? "a-talent" : "a-job"}
                     href="https://landing.jobs/"
                     target="_blank"
-                    rel="noopener noreferrer">
+                    rel="noopener noreferrer"
+                >
                     {" "}
                     Landing.Jobs{" "}
                 </a>
